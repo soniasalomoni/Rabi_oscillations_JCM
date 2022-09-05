4fcb5afc469d0c520e1da51fe5a08d6f864b20c2
Sonia Salomoni <sonia.salomoni@studio.unibo.it>
2022-09-05 14:10:23 +0200

    classes instantiation outside reading function

--- examples/ex_PDF_n/rabi.py
+++ examples/ex_PDF_n/rabi.py
@@ -8,8 +8,20 @@ import rabi_module as rabi
 print('\t ... ', end='\r')
 
 # reading
-simulation, saving_info = rabi.read_txt()
+field_info, atom_info, system_info, simulation_info, saving_info = rabi.read_txt()
+
+# unpacking
+AVG_N, PDF_N, CUT_N = field_info
+Cg_0, Ce_0 = atom_info
+OMEGA, DELTA = system_info
+TMAX, TSTEP = simulation_info
 SAVE_TXT, SAVE_PNG, OUT_LABEL = saving_info
+
+# creating objects
+field = rabi.Field(AVG_N,PDF_N,CUT_N)
+atom = rabi.Atom(Cg_0,Ce_0)
+system = rabi.System(field, atom, OMEGA, DELTA)
+simulation = rabi.Simulation(system,TMAX,TSTEP)
 
 # running
 simulation.run()
--- rabi_module/utilities/reading.py
+++ rabi_module/utilities/reading.py
@@ -9,6 +9,22 @@ import rabi_module as rabi
 # define reading function
 
 def read_txt():
+    """
+    Reads input parameters from a .txt file
+
+    Parameters:
+    -----------
+    simulation : Simulation 
+        the Simulation instance whose inversion function is saved
+    label : str
+        the name of the .txt output file
+    
+    Returns:
+    --------
+    read_info : tuple
+        a tuple containing all the readed information
+
+    """
     # initialize config 
     config = configparser.ConfigParser()
 
@@ -28,28 +44,29 @@ def read_txt():
     PDF_N = config.get('field','pdf_n')
     CUT_N = int(config.get('field','cut_n'))
 
+    field_info = (AVG_N, PDF_N, CUT_N)
+
     Cg_0 = float(config.get('atom','Cg'))
     Ce_0 = float(config.get('atom','Ce'))
+
+    atom_info = (Cg_0, Ce_0)
 
     OMEGA = float(config.get('interaction','int_coupling'))
     DELTA = float(config.get('interaction','int_detuning'))
 
+    system_info = (OMEGA, DELTA)
+
     TMAX = int(config.get('simulation','time'))
     TSTEP = float(config.get('simulation','step'))
+
+    simulation_info = (TMAX, TSTEP)
 
     SAVE_TXT = bool(config.get('output','save_txt'))
     SAVE_PNG = bool(config.get('output','save_png'))
     OUT_LABEL = config.get('output','out_label')
 
-    # -------------------------- #
-    # initialize class instances #
-    # -------------------------- #
-
-    field = rabi.Field(AVG_N,PDF_N,CUT_N)
-    atom = rabi.Atom(Cg_0,Ce_0)
-    system = rabi.System(field, atom, OMEGA, DELTA)
-    simulation = rabi.Simulation(system,TMAX,TSTEP)
-
     saving_info = (SAVE_TXT, SAVE_PNG, OUT_LABEL)
 
-    return simulation, saving_info
+    read_info = (field_info, atom_info, system_info, simulation_info, saving_info)
+
+    return read_info
